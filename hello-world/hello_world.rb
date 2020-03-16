f30311e73e751d6549018bfca7b1115fed0834ad
aaronprograms <aaron.eh.son@gmail.com>
2020-03-15 23:05:06 -0400

    Add completed exercises

---
+++ hello-world/hello_world.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Creates an HelloWorld Object
+class HelloWorld
+  def self.hello
+    'Hello, World!'
+  end
+end
---
+++ two-fer/two_fer.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+=begin
+Write your code for the 'Two Fer' exercise in this file. Make the tests in
+`two_fer_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/two-fer` directory.
+=end
+# "One for X, one for me."
+class TwoFer
+  def self.two_fer(name = 'you')
+    "One for #{name}, one for me."
+  end
+end
